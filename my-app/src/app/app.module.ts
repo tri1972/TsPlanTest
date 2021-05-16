8140e86e6b42dfa4d2c5f41b8c71bad1a3718aa7
tritri <sakaitri@gmail.com>
2021-05-16 11:56:59 +0900

    Component追加

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,19 +6,37 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserService } from './user/user.service';
+import { FormsModule } from '@angular/forms';
+import { ConfirmDialogComponent } from './tools/confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    DashboardComponent
+    DashboardComponent,
+    UserListComponent,
+    UserEditComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
+  entryComponents: [
+    ConfirmDialogComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- my-app/src/app/dashboard/dashboard.component.ts
+++ my-app/src/app/dashboard/dashboard.component.ts
@@ -5,11 +5,45 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+/*
+@Component({
+  selector: 'app-dashboard',
+  template: `
+    <!--イベントハンドラーを登録-->
+    <input type="button" value="現在時刻" (click)="onclick()" />
+    <div>{{result}}</div>
+  `,
+})
+*/
+export class DashboardComponent  {
+  message: string;
+  constructor() 
+  { 
+    this.message = 'This is a sample of Angular application.';
+  }
+  // 変数resultを初期化
+  result = '現在時刻は不明です。';
 
-  constructor() { }
+  // ボタンクリック時に現在時刻を表示
+  onclick() {
+    this.result = `現在時刻は、${new Date().toLocaleTimeString()}です。`;
+  }
+}
+/*
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  message: string;
+  constructor() 
+  { 
+    this.message = 'This is a sample of Angular application.';
+  }
 
   ngOnInit(): void {
   }
 
 }
+*/
