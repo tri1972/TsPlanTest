6b12ff5f9b53bd2d6ce6808f703204668eda301c
tritri <sakaitri@gmail.com>
2021-07-31 11:38:15 +0900

    サーバーアクセスのコードをmainコンポーネントに移動

--- my-app/src/app/main/main.component.ts
+++ my-app/src/app/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { AccountService, Configuration, RegisterAccount, TsService } from '../tsplanApi';
+import accountData from '../../Account.json';
 
 @Component({
   selector: 'app-main',
@@ -10,15 +13,72 @@ export class MainComponent implements OnInit {
   public parentData: string;
   public childData: string;
 
-  constructor() 
-  { 
+
+  public httpInstance: HttpClient;
+
+  constructor(private http: HttpClient) {
+    this.httpInstance = http;
   }
 
   ngOnInit(): void {
   }
 
-  onReceiveEventFromChild(eventData: string) {
+  async onReceiveEventFromChild(eventData: string) {
     this.childData = eventData;
+
+    var spiceNetList
+      = eventData;
+    //swaggerApi使用
+    var config = new Configuration();
+    config.username = "sakaitri@gmail.com";
+    config.password = "pxi13351";
+    config.basePath = 'https://tsplanning.azurewebsites.net';
+    //config.basePath='http://localhost:54248';
+
+    async function callTsPost(httpService: HttpClient, token: string) {
+      var configTsPost = new Configuration();
+      configTsPost.username = "sakaitri@gmail.com";
+      configTsPost.password = "pxi13351";
+      configTsPost.basePath = 'https://tsplanning.azurewebsites.net';
+      //configTsPost.basePath='http://localhost:54248';
+      configTsPost.accessToken = 'Bearer ' + token;
+      configTsPost.apiKeys = { "Authorization": 'Bearer ' + token }
+      var bodyTsSpiceNetListPost
+        = {
+        spiceNetList: spiceNetList,
+        temperature: [40]
+      };
+      var instance = await new TsService(httpService, null, configTsPost);
+      await instance.apiTsSpiceNetListPost(bodyTsSpiceNetListPost, 'body', true).subscribe({
+        next(position) {
+          console.log('Current Position: ', position);
+        },
+        error(msg) {
+          console.log('Error Getting Location: ', msg);
+        }
+      });
+    }
+
+    try {
+      var bodyAccountPost = { password: accountData.password, userName: accountData.username };
+      var instanceAccountService = new AccountService(this.http, null, config);
+      var localHttpInstance: HttpClient = this.httpInstance;
+      await instanceAccountService.apiAccountPost(bodyAccountPost, 'body', true).subscribe({
+        next(position) {
+          localHttpInstance
+          this.token = position.token;
+          this.expiresIn = position.expiresIn;
+          this.userName = position.userName;
+          callTsPost(localHttpInstance, position.token);
+        },
+        error(msg) {
+          console.log('Error Getting Location: ', msg);
+        }
+      });
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
 }
 
--- my-app/src/app/ts-plan-input/ts-plan-input.component.ts
+++ my-app/src/app/ts-plan-input/ts-plan-input.component.ts
@@ -8,9 +8,6 @@
  * https://www.atmarkit.co.jp/ait/articles/1708/16/news107.html
  */
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
-import { AccountService, Configuration, RegisterAccount, TsService } from '../tsplanApi';
-import accountData from '../../Account.json';
 import { Input } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 
@@ -26,69 +23,13 @@ export class TsPlanInputComponent implements OnInit {
   
   name = 'Tom';
   msg = '';
-  public httpInstance: HttpClient;
 
-  constructor(private http: HttpClient) {
-    this.httpInstance = http;
+  constructor() {
   }
 
   async onclick(value: string) {
-
-    var spiceNetList
-      = value;
-    //swaggerApi使用
-    var config = new Configuration();
-    config.username = "sakaitri@gmail.com";
-    config.password = "pxi13351";
-    config.basePath = 'https://tsplanning.azurewebsites.net';
-    //config.basePath='http://localhost:54248';
-
-    async function callTsPost(httpService: HttpClient, token: string) {
-      var configTsPost = new Configuration();
-      configTsPost.username = "sakaitri@gmail.com";
-      configTsPost.password = "pxi13351";
-      configTsPost.basePath = 'https://tsplanning.azurewebsites.net';
-      //configTsPost.basePath='http://localhost:54248';
-      configTsPost.accessToken = 'Bearer ' + token;
-      configTsPost.apiKeys = { "Authorization": 'Bearer ' + token }
-      var bodyTsSpiceNetListPost
-        = {
-        spiceNetList: spiceNetList,
-        temperature: [40]
-      };
-      var instance = await new TsService(httpService, null, configTsPost);
-      await instance.apiTsSpiceNetListPost(bodyTsSpiceNetListPost, 'body', true).subscribe({
-        next(position) {
-          console.log('Current Position: ', position);
-        },
-        error(msg) {
-          console.log('Error Getting Location: ', msg);
-        }
-      });
-    }
-
-    try {
-      var bodyAccountPost = { password: accountData.password, userName: accountData.username };
-      var instanceAccountService = new AccountService(this.http, null, config);
-      var localHttpInstance: HttpClient = this.httpInstance;
-      await instanceAccountService.apiAccountPost(bodyAccountPost, 'body', true).subscribe({
-        next(position) {
-          localHttpInstance
-          this.token = position.token;
-          this.expiresIn = position.expiresIn;
-          this.userName = position.userName;
-          callTsPost(localHttpInstance, position.token);
-        },
-        error(msg) {
-          console.log('Error Getting Location: ', msg);
-        }
-      });
-    }
-    catch (err) {
-      console.log(err);
-    }
     this.event.emit(
-      '子コンポーネントから親コンポーネントへデータを渡す際はイベントを経由します。'
+      value
     );
   }
 
